d795a6995a96592e7eee91a01bdec5277d14f192
spaceguythe1 <126317944+spaceguythe1@users.noreply.github.com>
2026-05-18 17:03:14 +0100

    Add files via upload

--- install.py
+++ install.py
@@ -10,6 +10,7 @@ print()
 print("Do you acknowledge this and want to continue installation? (y/n)")
 startchec = input()
 if(startchec == "y"):
+    os.chdir("/")
     print("starting install...")
     time.sleep(0.05)
     os.system("pacman -S sudo")
@@ -22,6 +23,7 @@ if(startchec == "y"):
     os.system(f"usermod -aG wheel {userwsudo}")
     os.system(f"passwd {userwsudo}")
     time.sleep(0.1)
+    os.system(f"mkdir /home/{userwsudo}/.config")
     print("created your user!")
     time.sleep(0.1)
 
@@ -37,7 +39,7 @@ if(startchec == "y"):
 
     # fastfetch
     os.system(f"mkdir /home/{userwsudo}/.config/fastfetch && mv /root/archerthefact/ffconfig/* /home/{userwsudo}/.config/fastfetch/")
-    os.system("sed -i 'fastfetch --logo-color-1 white --logo-color-2 red --color red' ~/.bashrc")
+    os.system(f"sed -i 'fastfetch --logo-color-1 white --logo-color-2 red --color red' /home/{userwsudo}/.bashrc")
 
 
     # kitty
@@ -49,9 +51,12 @@ if(startchec == "y"):
     print("Installing utils by me...")
     time.sleep(0.1)
     os.system("rm -rf ~/tarm /usr/local/bin/tarm && git clone https://github.com/spaceguythe1/tarm.git && mv ~/tarm/tarm /usr/local/bin && chmod +x /usr/local/bin/tarm && rm -rf ~/tarm")
+    os.system("chmod +x /usr/local/bin/tarm")
+    os.system("rm -rf ~/tarm")
 
 if(startchec == "y"):
-    os.system("systemctl --enable sddm.service")
+    os.system("systemctl enable sddm.service")
+    os.system(f"chown -R {userwsudo}:{userwsudo} /home/{userwsudo}")
     time.sleep(0.05)
     print()
     print()
